9931ded32beb65778f097111c741df01c60571f9
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2023-03-21 21:01:25 -0600

    Pass tests in index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+customerName = "bob";
+const leastFavoriteCustomer = "Joe";
+
+function upperCaseCustomerName(name) {
+	customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+	bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+	bestCustomer = "maybe bob";
+}
+
+function changeLeastFavoriteCustomer() {
+	leastFavoriteCustomer = "Bruce";
+}
